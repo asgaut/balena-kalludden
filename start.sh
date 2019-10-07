b2efc06520ff572057f01b4f268d994feb492834
Asgaut Eng <asgaut@gmail.com>
2019-10-07 19:30:19 +0200

    Build own version of librtlsdr
    Because of this problem:
    https://github.com/steve-m/librtlsdr/commit/81833a1cf6288fee93a9157c0f60cafb5ec340b9

--- start.sh
+++ start.sh
@@ -3,3 +3,6 @@ echo "RTL_433_INPUT:" $RTL_433_INPUT
 echo "RTL_433_OUTPUT:" $RTL_433_OUTPUT
 echo "Starting rtl_433..."
 rtl_433 $RTL_433_INPUT $RTL_433_OUTPUT
+echo "rtl_433 exited..."
+while true; do
+done
