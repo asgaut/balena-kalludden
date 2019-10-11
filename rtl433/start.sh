256371eca74141f9edd48dc2e8bd2d81f1e75658
Asgaut Eng <asgaut@gmail.com>
2019-10-11 21:47:50 +0200

    Remove the RTL-SDR kernel module if loaded

--- rtl433/start.sh
+++ rtl433/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "RTL_433_INPUT:" $RTL_433_INPUT
 echo "RTL_433_OUTPUT:" $RTL_433_OUTPUT
+echo "Removing RTL-SDR kernel module"
+rmmod dvb_usb_rtl28xxu
 echo "Starting rtl_433..."
 rtl_433 $RTL_433_INPUT $RTL_433_OUTPUT
 echo "rtl_433 exited..."
